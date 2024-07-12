bd50d3fba3a7adbb3f09e2a140e0c84a4276690a
zhang <colorfulshadowty@gmail.com>
2024-07-12 18:32:08 +0800

    fix bugs and add new feature: AllKill

--- main.py
+++ main.py
@@ -1,80 +1,101 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = 'Jerry'
+
 import datetime
 from tkinter import *
 from seckill.seckill_taobao import ChromeDrive
 
 
-
-def run_killer(txt, txt2):
+def run_killer(txt, txt2, option, link=None):
     seckill_time = txt.get()
     password = str(txt2.get())
-    print(seckill_time, password)
-    ChromeDrive(seckill_time = seckill_time, password = password).sec_kill()
+    option_value = option.get()
+    link_value = link.get() if link else ''
+    print(seckill_time, password, option_value, link_value)
+    # Adjust this part to handle the link if necessary
+    if option_value == '1':
+        ChromeDrive(seckill_time=seckill_time, password=password).sec_kill()
+    elif option_value == '2':
+        ChromeDrive(seckill_time=seckill_time, password=password, link=link_value).AllKill()
 
+
+def on_option_change(option, lbl_link, link_entry):
+    if option.get() == '2':
+        lbl_link.grid(column=0, row=3, padx=10, pady=10, sticky=E)
+        link_entry.grid(column=1, row=3, padx=10, pady=10)
+    else:
+        lbl_link.grid_forget()
+        link_entry.grid_forget()
 
 
 def main():
     win = Tk()
     win.title('小熊秒杀助手')
-    width = 380
-    height = 300
+
+    lbl = Label(win, text="开抢时间：", width=10, height=2, bg='#f0f0f0', font=('Arial', 12))
+    lbl.grid(column=0, row=0, padx=10, pady=10, sticky=E)
+    start_time = StringVar()
+    txt = Entry(win, textvariable=start_time, width=25, font=('Arial', 12))
+    txt.grid(column=1, row=0, padx=10, pady=10)
+    start_time.set(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+    lbl2 = Label(win, text="支付密码：", width=10, height=2, bg='#f0f0f0', font=('Arial', 12))
+    lbl2.grid(column=0, row=1, padx=10, pady=10, sticky=E)
+    txt2 = Entry(win, width=25, show='*', font=('Arial', 12))
+    txt2.grid(column=1, row=1, padx=10, pady=10)
+
+    option = StringVar()
+    option.set('1')
+
+    lbl3 = Label(win, text="选项：", width=10, height=2, bg='#f0f0f0', font=('Arial', 12))
+    lbl3.grid(column=0, row=2, padx=10, pady=10, sticky=E)
+
+    lbl_link = Label(win, text="抢购链接：", width=10, height=2, bg='#f0f0f0', font=('Arial', 12))
+    link_entry = Entry(win, width=25, font=('Arial', 12))
+    # Initially hidden, will be shown when option 2 is selected
+
+    r1 = Radiobutton(win, text='选项1', variable=option, value='1', bg='#f0f0f0', font=('Arial', 12),
+                     command=lambda: on_option_change(option, lbl_link, link_entry))
+    r1.grid(column=1, row=2, sticky=W, padx=10, pady=5)
+    r2 = Radiobutton(win, text='选项2', variable=option, value='2', bg='#f0f0f0', font=('Arial', 12),
+                     command=lambda: on_option_change(option, lbl_link, link_entry))
+    r2.grid(column=1, row=2, sticky=W, padx=100, pady=5)
+
+    b1 = Button(win, text='开始',
+                command=lambda: run_killer(txt, txt2, option, link_entry if option.get() == '2' else None))
+    b1.config(font='Helvetica -12 bold', bg='#ff5c5c', fg='white', relief='raised', width=10, height=2)
+    b1.grid(column=0, row=4, columnspan=2, pady=20)
+    win.resizable(width=False, height=False)
+
+    instructions = [
+        '使用说明:',
+        '1、安装chrome浏览器以及chromeDriver',
+        '2、如果选择SecKill，则抢购前要清空购物车，然后把要抢的东西加入购物车，如果选择AnyKill, 则需要填写想要购买的商品的链接',
+        '3、开抢时间必须是 %Y-%m-%d %H:%M:%S 形式，如2020-12-29 12:10:15',
+        '4、输入开抢时间和支付密码后点开始，程序会控制浏览器打开淘宝登陆页',
+        '5、扫码登陆后，程序会自动刷新购物车页面，到点会完成抢购动作',
+        '6、本项目仅供交流学习使用，请勿用于其它任何商业用途',
+        '7、如果想手动付款，输入开抢时间后不用输入支付密码，直接点开始就可以了'
+    ]
+
+    txt0 = Label(win, text=instructions[0], width=10, height=2, bg='#f0f0f0', font=('Arial', 12, 'bold'))
+    txt0.grid(column=0, row=5, padx=10, pady=5, columnspan=2, sticky=W)
+
+    for i, instruction in enumerate(instructions[1:], start=6):
+        lbl_instruction = Label(win, text=instruction, bg='#f0f0f0', font=('Arial', 10), wraplength=450, justify=LEFT)
+        lbl_instruction.grid(column=0, row=i, padx=10, pady=2, columnspan=2, sticky=W)
+
+    win.update_idletasks()
+    width = win.winfo_width()
+    height = win.winfo_height()
     screenwidth = win.winfo_screenwidth()
     screenheight = win.winfo_screenheight()
     alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
     win.geometry(alignstr)
 
-    lbl = Label(win, text = "开抢时间：", width = 8, height = 2)
-    lbl.grid(column = 0, row = 0)
-    start_time = StringVar()
-    txt = Entry(win, textvariable = start_time, width = 18)
-    txt.grid(column = 1, row = 0)
-    start_time.set(str(datetime.datetime.now()))
-
-    lbl2 = Label(win, text = "支付密码：", width = 8, height = 2)
-    lbl2.grid(column = 0, row = 1)
-    txt2 = Entry(win, width = 18, show = '*')
-    txt2.grid(column = 1, row = 1)
-
-    b1 = Button(win, text = '开始', command = lambda: run_killer(txt, txt2))
-    b1.config(font = 'Helvetica -10 bold', bg = 'red', relief = 'sunken', width = 8, height = 6)
-    b1.place(x=300, y=5)
-    win.resizable(width = False, height = False)
-
-    txt0 = Label(win, text = '使用说明:',width = 8, height = 2)
-    txt0.grid(column = 0, row = 3)
-
-    txt3 = Label(win, text = '1、安装chrome浏览器以及chromeDriver')
-    txt3.config(font = 'Helvetica -10 bold', fg = 'red')
-    txt3.place(x = 10, y = 120)
-
-    txt4 = Label(win, text = '2、抢购前要清空购物车，然后把要抢的东西加入购物车')
-    txt4.config(font = 'Helvetica -10 bold', fg = 'red')
-    txt4.place(x = 10, y = 140)
-
-    txt5 = Label(win, text = '3、开抢时间必须是 %Y-%m-%d %H:%M:%S 形式，如2020-12-29 12:10:15' )
-    txt5.config(font = 'Helvetica -10 bold', fg = 'red')
-    txt5.place(x=10, y=160)
-
-    txt6 = Label(win, text = '4、输入开抢时间和支付密码后点开始，程序会控制浏览器打开淘宝登陆页')
-    txt6.config(font = 'Helvetica -10 bold', fg = 'red')
-    txt6.place(x = 10, y = 180)
-
-    txt7 = Label(win, text = '5、扫码登陆后，程序会自动刷新购物车页面，到点会完成抢购动作')
-    txt7.config(font = 'Helvetica -10 bold', fg = 'red')
-    txt7.place(x = 10, y = 200)
-
-    txt8 = Label(win, text = '6、本项目仅供交流学习使用，请勿用于其它任何商业用途')
-    txt8.config(font = 'Helvetica -10 bold', fg = 'red')
-    txt8.place(x = 10, y = 220)
-
-    txt9 = Label(win, text = '7、如果想手动付款，输入开抢时间后不用输入支付密码，直接点开始就可以了')
-    txt9.config(font = 'Helvetica -10 bold', fg = 'red')
-    txt9.place(x = 10, y = 240)
     win.mainloop()
 
 
 if __name__ == '__main__':
     main()
-    # print(datetime.date.today())
--- seckill/seckill_taobao.py
+++ seckill/seckill_taobao.py
@@ -20,14 +20,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
+import tkinter as tk
+from tkinter import ttk
 
 # 抢购失败最大次数
 max_retry_count = 30
 
 
 def default_chrome_path():
-
     driver_dir = getattr(utils_settings, "DRIVER_DIR", None)
     if platform.system() == "Windows":
         if driver_dir:
@@ -41,19 +41,51 @@ def default_chrome_path():
         raise Exception("The chromedriver drive path attribute is not found.")
 
 
+def parse_sku_wrap(lines):
+    lines = lines.split("\n")
+    result = {}
+    # 初始化变量
+    key = None
+    i = 0
+    while i < len(lines):
+        item = lines[i]
+        if item == '数量':
+            break
+        if item == '：':
+            i += 1
+            continue
+        elif i + 1 < len(lines) and lines[i + 1] == '：':
+            # 当前item是key
+            key = item
+            result[key] = []  # 为key初始化一个空列表
+            i += 2  # 跳过下一个冒号
+        else:
+            # 当前item是value
+            if key is not None:
+                result[key].append(item)
+            i += 1
+    return result
+
+
 class ChromeDrive:
 
-    def __init__(self, chrome_path=default_chrome_path(), seckill_time=None, password=None):
+    def __init__(self, chrome_path=default_chrome_path(), seckill_time=None, password=None, link=None):
         self.chrome_path = chrome_path
         self.seckill_time = seckill_time
         self.seckill_time_obj = datetime.strptime(self.seckill_time, '%Y-%m-%d %H:%M:%S')
         self.password = password
+        self.link = link
+        self.selected_options = None
+        #self.driver = webdriver.Chrome(executable_path=self.chrome_path)
 
     def start_driver(self):
         try:
             driver = self.find_chromedriver()
-        except WebDriverException:
-            print("Unable to find chromedriver, Please check the drive path.")
+            if driver is None:
+                raise WebDriverException("Unable to find chromedriver.")
+        except WebDriverException as e:
+            print(f"Unable to find chromedriver: {e}. Please check the drive path.")
+            return None
         else:
             return driver
 
@@ -74,7 +106,8 @@ class ChromeDrive:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.accept_untrusted_certs = True
         chrome_options.assume_untrusted_cert_issuer = True
-        arguments = ['--no-sandbox', '--disable-impl-side-painting', '--disable-setuid-sandbox', '--disable-seccomp-filter-sandbox',
+        arguments = ['--no-sandbox', '--disable-impl-side-painting', '--disable-setuid-sandbox',
+                     '--disable-seccomp-filter-sandbox',
                      '--disable-breakpad', '--disable-client-side-phishing-detection', '--disable-cast',
                      '--disable-cast-streaming-hw-encoding', '--disable-cloud-import', '--disable-popup-blocking',
                      '--ignore-certificate-errors', '--disable-session-crashed-bubble', '--disable-ipv6',
@@ -84,23 +117,22 @@ class ChromeDrive:
         chrome_options.add_argument(f'--user-agent={choice(get_useragent_data())}')
         return chrome_options
 
-    def login(self, login_url: str="https://www.taobao.com"):
+    def login(self, login_url: str = "https://www.taobao.com"):
         if login_url:
             self.driver = self.start_driver()
         else:
             print("Please input the login url.")
             raise Exception("Please input the login url.")
 
-
         while True:
             self.driver.get(login_url)
             try:
-                if self.driver.find_element_by_link_text("亲，请登录"):
+                if self.driver.find_element(By.LINK_TEXT, "亲，请登录"):
                     print("没登录，开始点击登录按钮...")
-                    self.driver.find_element_by_link_text("亲，请登录").click()
+                    self.driver.find_element(By.LINK_TEXT, "亲，请登录").click()
                     print("请在30s内扫码登陆!!")
                     sleep(30)
-                    if self.driver.find_element_by_xpath('//*[@id="J_SiteNavMytaobao"]/div[1]/a/span'):
+                    if self.driver.find_element(By.XPATH, '//*[@id="J_SiteNavMytaobao"]/div[1]/a/span'):
                         print("登陆成功")
                         break
                     else:
@@ -124,14 +156,13 @@ class ChromeDrive:
                 print("抢购时间点将近，停止自动刷新，准备进入抢购阶段...")
                 break
 
-
     def sec_kill(self):
         self.keep_wait()
         self.driver.get("https://cart.taobao.com/cart.htm")
         sleep(1)
 
-        if self.driver.find_element_by_id("J_SelectAll1"):
-            self.driver.find_element_by_id("J_SelectAll1").click()
+        if self.driver.find_element(By.ID, "J_SelectAll1"):
+            self.driver.find_element(By.ID, "J_SelectAll1").click()
             print("已经选中全部商品！！！")
 
         submit_succ = False
@@ -152,14 +183,14 @@ class ChromeDrive:
 
                 try:
 
-                    if self.driver.find_element_by_id("J_Go"):
-                        self.driver.find_element_by_id("J_Go").click()
+                    if self.driver.find_element(By.ID, "J_Go"):
+                        self.driver.find_element(By.ID, "J_Go").click()
                         print("已经点击结算按钮...")
                         click_submit_times = 0
                         while True:
                             try:
                                 if click_submit_times < 10:
-                                    self.driver.find_element_by_link_text('提交订单').click()
+                                    self.driver.find_element(By.LINK_TEXT, '提交订单').click()
                                     print("已经点击提交订单按钮")
                                     submit_succ = True
                                     break
@@ -179,10 +210,132 @@ class ChromeDrive:
             if self.password:
                 self.pay()
 
+    def AllKill(self):
+        self.login()
+        self.configure_options()
+        print(self.selected_options)
+        while not self.monitor_options():
+            self.driver.refresh()
+            sleep(1)
+        self.buy_on_page()
+
+    def buy_on_page(self):
+        for option, value in self.selected_options.items():
+            if option == '数量':
+                try:
+                    quantity_input = WebDriverWait(self.driver, 10).until(
+                        EC.presence_of_element_located((By.CLASS_NAME, 'Operation--countValue--3VF_tPH'))
+                    )
+                    quantity_input.clear()
+                    quantity_input.send_keys(value)
+                except Exception as e:
+                    print(f"Error setting quantity: {e}")
+                continue
+
+            try:
+                element = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, f"//span[@title='{value}']"))
+                )
+                parent_element = element.find_element(By.XPATH, "..")
+                if "SkuContent--isSelected" not in parent_element.get_attribute("class"):
+                    element.click()
+            except Exception as e:
+                print(f"Error clicking on option {option} with value {value}: {e}")
+
+        try:
+            buy_now_button = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, "//button/span[text()='立即购买']"))
+            )
+            buy_now_button.click()
+        except Exception as e:
+            print(f"Error clicking on the '立即购买' button: {e}")
+
+        click_submit_times = 0
+        while True:
+            try:
+                if click_submit_times < 10:
+                    self.driver.find_element(By.LINK_TEXT, '提交订单').click()
+                    print("已经点击提交订单按钮")
+                    submit_succ = True
+                    break
+                else:
+                    print("提交订单失败...")
+            except Exception as e:
+                print("没发现提交按钮, 页面未加载, 重试...")
+                click_submit_times = click_submit_times + 1
+                sleep(0.1)
+            sleep(0.1)
+        if submit_succ:
+            if self.password:
+                self.pay()
+
+    def monitor_options(self):
+        all_available = True
+        for option, value in self.selected_options.items():
+            if option == '数量':
+                continue
+
+            option_elements = self.driver.find_elements(By.XPATH,
+                                                        f"//span[text()='{option}']/../following-sibling::div//span[@title='{value}']")
+            if not option_elements:
+                print(f"Option {option} with value {value} not found.")
+                all_available = False
+                break
+            option_element = option_elements[0]
+            parent_class = option_element.find_element(By.XPATH, "..").get_attribute("class")
+            if 'isDisabled' in parent_class:
+                print(f"Option {option} with value {value} is disabled.")
+                all_available = False
+                break
+        return all_available
+
+    def configure_options(self):
+        def on_submit():
+            selected_options = {key: vars[key].get() for key in vars}
+            quantity = quantity_var.get()
+            print("选择的配置: ", selected_options)
+            print(f"填写的数量: {quantity}")
+            self.selected_options = selected_options
+            self.selected_options['数量'] = quantity
+            config_window.destroy()
+
+        self.driver.get(self.link)
+        if self.driver.find_element(By.ID, "skuWrap"):
+            skuWrap_text = self.driver.find_element(By.ID, "skuWrap").text
+            options = parse_sku_wrap(skuWrap_text)
+
+        config_window = tk.Toplevel()
+        config_window.title("选择配置")
+
+        vars = {}
+        row = 0
+        for option, values in options.items():
+            if values:
+                var = tk.StringVar()
+                ttk.Label(config_window, text=f"{option}:").grid(column=0, row=row, padx=10, pady=5)
+                combobox = ttk.Combobox(config_window, textvariable=var)
+                combobox['values'] = values
+                combobox.grid(column=1, row=row, padx=10, pady=5)
+                vars[option] = var
+                row += 1
+
+        quantity_var = tk.StringVar()
+        ttk.Label(config_window, text="数量:").grid(column=0, row=row, padx=10, pady=5)
+        quantity_entry = ttk.Entry(config_window, textvariable=quantity_var)
+        quantity_entry.grid(column=1, row=row, padx=10, pady=5)
+
+        submit_button = ttk.Button(config_window, text="提交", command=on_submit)
+        submit_button.grid(column=1, row=row + 1, padx=10, pady=10)
+
+        config_window.grab_set()
+        config_window.wait_window()
+
+        return self.selected_options
 
     def pay(self):
         try:
-            element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'sixDigitPassword')))
+            element = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'sixDigitPassword')))
             element.send_keys(self.password)
             WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, 'J_authSubmit'))).click()
             notify_user(msg="付款成功")
@@ -192,9 +345,8 @@ class ChromeDrive:
             sleep(60)
             self.driver.quit()
 
-
     def get_cookie(self):
         cookies = self.driver.get_cookies()
         cookie_json = json.dumps(cookies)
-        with open('./cookies.txt', 'w', encoding = 'utf-8') as f:
+        with open('./cookies.txt', 'w', encoding='utf-8') as f:
             f.write(cookie_json)
